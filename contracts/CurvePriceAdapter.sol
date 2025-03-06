99b0bfd682d7a1db70cde4a9c272662bc8476ac2
samclassix <samclassix@proton.me>
2025-03-06 13:14:53 +0100

    feat: curve pool, oracle_price

--- contracts/CurvePriceAdapter.sol
+++ contracts/CurvePriceAdapter.sol
@@ -1,30 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {Ownable} from '@openzeppelin/contracts/access/Ownable.sol';
+import {IMinimalAggregatorV3Interface} from './interfaces/IMinimalAggregatorV3Interface.sol';
+import {AggregatorV3Interface} from './interfaces/AggregatorV3Interface.sol';
 
 interface ICurvePool {
-	function get_dy(int128 i, int128 j, uint256 dx) external view returns (uint256);
+	function price_oracle() external view returns (uint256);
 }
 
-interface IOracle {
-	function getPrice() external view returns (uint256);
-}
-
-contract CurvePriceAdapter is IOracle, Ownable {
+contract ZCHFUSDTCurvePriceAdapter is IMinimalAggregatorV3Interface {
 	ICurvePool public immutable curvePool;
-	int128 public immutable tokenIndexA;
-	int128 public immutable tokenIndexB;
-	uint256 public immutable amountIn; // Amount of Token A used for price calculation
 
-	constructor(address _curvePool, int128 _tokenIndexA, int128 _tokenIndexB, uint256 _amountIn) Ownable(msg.sender) {
+	constructor(address _curvePool) {
 		curvePool = ICurvePool(_curvePool);
-		tokenIndexA = _tokenIndexA;
-		tokenIndexB = _tokenIndexB;
-		amountIn = _amountIn;
 	}
 
-	function getPrice() external view override returns (uint256) {
-		return curvePool.get_dy(tokenIndexA, tokenIndexB, amountIn);
+	function decimals() external pure override returns (uint8) {
+		return 18;
+	}
+
+	function description() external pure override returns (string memory) {
+		return 'zCHF/USDT exchange rate';
+	}
+
+	function latestRoundData()
+		external
+		view
+		override
+		returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+	{
+		return (0, int256(curvePool.price_oracle()), 0, 0, 0);
 	}
 }
